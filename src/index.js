1434431d82ab0d7b50f620c3f3eae264b42926ec
Jamezjaz <jamezjaz@gmail.com>
2020-10-28 20:07:12 -0500

    docs: displays the question

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const questions =[
-    {
-      question: 'What is 2 + 2',
-      answers: [
-        { text: '4', correct: true},
-        { text: '22', correct: false}
-      ]
-    }
-  ]
+const questions = [
+  {
+    question: 'What is 2 + 2',
+    answers: [
+      { text: '4', correct: true},
+      { text: '22', correct: false}
+    ]
+  }
+]
 const startBtn = document.querySelector('#start-btn');
 const questionCon = document.querySelector('#question-con');
 const questionElement = document.querySelector('#question');
 const answerBtnsElement = document.querySelector('#answer-btns');
+let shuffledQuestions, currentQuestionIndex;
 
 startBtn.addEventListener('click', () => {
   startGame();
@@ -19,6 +20,20 @@ startBtn.addEventListener('click', () => {
 const startGame = () => {
   console.log('Jamesss');
   startBtn.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random());
+  currentQuestionIndex = 0;
   questionCon.classList.remove('hide');
   nextQuestion();
+};
+
+const nextQuestion = () => {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+const showQuestion = (question) => {
+  questionElement.innerText = question.question;
+}
+
+const selectAnswer = () => {
+  
 };
